e7ad01b49a1e4de415102555a7de40f7c94cc1d9
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2016-05-02 14:14:59 +0100

    Add MDM support

--- share/hybrid/gpu-manager.c
+++ share/hybrid/gpu-manager.c
@@ -2915,12 +2915,12 @@ static bool enable_prime(const char *prime_settings,
     bool prime_discrete_on = false;
     bool prime_action_on = false;
 
-    /* We only support Lightdm and GDM at this time */
-    if (!(is_lightdm_default() || is_gdm_default() || is_sddm_default())) {
-        fprintf(log_handle, "Neither Lightdm nor GDM is the default display "
-                            "manager. Nothing to do\n");
-        return false;
-    }
+    // /* We only support Lightdm and GDM at this time */
+    // if (!(is_lightdm_default() || is_gdm_default() || is_sddm_default())) {
+    //     fprintf(log_handle, "Neither Lightdm nor GDM is the default display "
+    //                         "manager. Nothing to do\n");
+    //     return false;
+    // }
 
     /* Check the driver version
      * Note: this won't be available when the discrete GPU
